05fd812e93ea527a9c665678991f185c01102427
Domiq24 <dominikpolitylo@gmail.com>
2026-04-25 16:10:09 +0200

    Update config.ts

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -4,7 +4,7 @@ export const config = {
     port: process.env.PORT || 3100,
     database: "2024_politylo_dominik",
     dbUser: "2024_politylo_dominik",
-    dbPassword: "P0litylo24",
+    dbPassword: "",
     dbHost: "195.150.230.208",
     dbPort: 5432,
 };
